2e844140bb1cecc8d5dac9710e6c5e123dec7115
KatoCode23 <Kalen23Imani@gmail.com>
2016-05-18 23:03:19 -0400

    Dynamic Web Development Lists

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("HELLO!");
+//This function takes one parameter using jquery that takes in a string.
+function writeToWeightLoss(message){
+  $("#weightloss-list").append("<div>" + message + "</div>");
+}
+
+var weightlossArray = ["Jogging", "Sprinting", "Boxing", "Swimming", "Jumproping", "Walking"];
+for (var i=0; i < weightlossArray.length; i ++){
+  writeToWeightLoss(weightlossArray[i]);
+
+}
+function writeToGainMuscle(message){
+  $("#gainmuscle-list").append("<div>" + message + "</div>");
+}
+
+var gainMuscleArray = ["Squat", "Deadlift", "BenchPress", "MilitaryPress", "Pull-Ups", "Push-Ups"];
+for (var i=0; i < gainMuscleArray.length; i ++){
+  writeToGainMuscle(gainMuscleArray[i]);
+
+}
