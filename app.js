b619b397fc748e8b01e9dab7e26f25461c1cbf32
KatoCode23 <Kalen23Imani@gmail.com>
2016-11-03 16:17:44 -0400

    ng-click is in full affect

--- app.js
+++ app.js
@@ -2,4 +2,10 @@ var app = angular.module('app', []);
 
 app.controller('myController', function($scope){
 
+$scope.categoryList = ['about', 'skills', 'events', 'gratitude', 'gallery', 'words of wisdom', 'favorite things', 'contact'];
+
+$scope.categoryClick = function() {
+  console.log('Hello there sir Ive just been clicked');
+}
+
 });
