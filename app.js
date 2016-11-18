0bf22db32efb9774fc44274ad4e0f9ae1861fa30
KatoCode23 <Kalen23Imani@gmail.com>
2016-11-18 15:34:24 -0500

    Working on improving data structure

--- app.js
+++ app.js
@@ -5,10 +5,29 @@ $scope.currentCategory = "";
 
 $scope.categoryList = [
   {name: 'about'},
-  {name: 'skills', talent: [
-  {name: 'International Event Coordination'},
-  {name: 'Social Media Stradegy'}
-]},
+  {
+  name: 'skills',
+    talent: [
+       {name: 'International Event Coordination'},
+       {name: 'Social Media Srategy'},
+       {name: 'Branding Development'},
+       {name: 'Wedding Planner'},
+       {name: 'Brand Colaberation'},
+       {name: 'Fundraiser'},
+       {name: 'Cooperate Events Dinner Parties Fashion Shows Premiers'},
+       {name: 'Creative Consulting'},
+       {name: 'Publicity'},
+       {name: 'Photo and Gallery Production'},
+       {name: 'Book and Speaking Agent'},
+       {name: 'Sponsorship'},
+     ]
+ }
+ {
+  name: 'International Event Coordination'
+ },
+ {
+   name: 'Social Media Stradegy'}
+},
   {name: 'events'},
   {name: 'gratitude'},
   {name: 'gallery'},
