f1d43dfeadce9bfb03e88ba4a9220d87e7a26154
KatoCode23 <Kalen23Imani@gmail.com>
2016-11-17 21:30:24 -0500

    Starting to build drop down menu objects

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var app = angular.module('app', []);
 app.controller('myController', function($scope){
 
 $scope.categoryList = ['about', 'skills', 'events', 'gratitude', 'gallery', 'words of wisdom', 'favorite things', 'contact'];
+$scope.skills = [
+  {name: 'International Event Coordination'},
+  {name: 'Social Media Strategy'}
+];
 
 $scope.categoryClick = function() {
   console.log('Hello there sir Ive just been clicked');
