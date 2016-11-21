37dce711253c31f8e6e59cf4a03a089dd56e84f5
KatoCode23 <Kalen23Imani@gmail.com>
2016-11-21 18:05:04 -0500

    Pics in correct order text align left

--- app.js
+++ app.js
@@ -65,6 +65,11 @@ $scope.testimonialGallery = [
       description: 'Its such a pleasure to get to work with Noel & her team. Create Your Dream Year was a great success and we couldnt have done it without her! She is professional, creative, and passionate about what she does­ making what could be a stressful experience, quite a pleasure!'
     },
     {
+      name: 'Meggan Watterson, Best Selling Author, Speaker, & Founder of REVEAL',
+      image: 'NoellePuck/testimonial5.jpg',
+      description: 'Noel is an angel & an organizational ninja all rolled into one beautiful package. She is so masterful at creating the flow of an event that I am left to do what I do best without being distracted by the details that used to overwhelm me. If you are looking to create a flawless event, big or small, look no further! Noel Elie Productions is it!'
+    },
+    {
       name: 'Terri Cole, LCSW Licensed Psychotherapist, Speaker, and Transformation Coach',
       image: 'NoellePuck/testimonial3.png',
       description: 'Noel makes me feel so entirely provided for! I never worried once during any of my events. She took care of absolutely every detail which allowed me to just be fully present and entirely there­ a rare and invaluable gift'
@@ -73,12 +78,9 @@ $scope.testimonialGallery = [
       name: 'Joel Readence, Lifecoach',
       image: 'NoellePuck/testimonial4.jpg',
       description: 'Working a full time job and growing a coaching practice allowed me ZERO time to plan and execute a charity fundraiser for 250+ people. Hiring Noel was the best possible thing I could have done! Her in­depth knowledge of event planning and execution, ability to negotiate contracts and pricing, target and enlist sponsors, and utilize her extensive network of connections ensured a VERY successful event and a new fundraising record for my charity fund! Whether it be a book launch, a fundraiser, or a client retreat, I will never have another event without Noel’s involvement!'
-    },
-    {
-      name: 'Meggan Watterson, Best Selling Author, Speaker, & Founder of REVEAL',
-      image: 'NoellePuck/testimonial5.jpg',
-      description: 'Noel is an angel & an organizational ninja all rolled into one beautiful package. She is so masterful at creating the flow of an event that I am left to do what I do best without being distracted by the details that used to overwhelm me. If you are looking to create a flawless event, big or small, look no further! Noel Elie Productions is it!'
     }
+
+
 
 ];
 
