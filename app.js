2813114fe113f1ce24fd149349ea36772b882555
KatoCode23 <Kalen23Imani@gmail.com>
2016-12-22 00:32:37 -0500

    Object set up for Eric

--- app.js
+++ app.js
@@ -2,4 +2,37 @@ var app = angular.module('app', []);
 
 app.controller('myController', function($scope){
 
+  scope.eleenHalvorsen = [
+    {
+      name: 'Eric Pringle'
+    },
+    {
+     jobTitle: 'CEO and Founder of the "Give a Glove Foundation"'
+    }
+
+  ];
+
+  $scope.menu = [
+    {
+      category: 'Upcoming Events'
+    },
+    {
+      category: 'Projects'
+    },
+    {
+      category: 'Trivia and Raffle'
+    },
+    {
+      category: 'Testimonials'
+    },
+    {
+      category: 'Gloves Given'
+    },
+    {
+      category: 'Mantra/Mission Statement'
+    },
+
+  ];
+
+
 });
