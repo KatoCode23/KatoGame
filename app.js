cee2861697e7ffde950dee07ffc4072388966f5c
KatoCode23 <Kalen23Imani@gmail.com>
2017-01-07 23:38:42 -0500

    Making Progress

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = angular.module('app', []);
 app.controller('myController', function($scope){
 $scope.currentCategory = {name: "none"};
 $scope.currentTeam = {name: "none"};
-var numberOfImages= 33;
+var numberOfImages= 32;
 var currentImage= null;
 $scope.categoryList = [
   {
@@ -64,7 +64,13 @@ $scope.categoryList = [
     ]
  },
  {
-   name: 'gallery'
+   name: 'gallery',
+    talent: [
+      {
+        name: 'gallery',
+        link: 'gallery.html'
+      }
+    ]
  },
  {
    name: 'words of wisdom'
@@ -80,32 +86,32 @@ $scope.categoryList = [
 $scope.testimonialGallery = [
     {
       name: 'Kris Carr, New York Times best Selling Author',
-      image: 'NoellePuck/testimonial0.jpg',
+      image: 'NoelPuck/testimonial0.jpg',
       description: 'Working with Noel is a dream come true! Shes an impeccable event producer whos detail oriented, supportive, and forward­ thinking. When I work with Noel, Im able to focus on what matters most, delivering my message. She takes care of the rest!'
     },
     {
       name: 'Latham Thomas, Speaker, Author, & Founder of Mama Glow',
-      image: 'NoellePuck/testimonial1.png',
+      image: 'NoelPuck/testimonial1.png',
       description: 'Noel Elie is the goddess of masterful organization and thoughtful execution. She pays such incredible attention to detail and has a remarkable way of keeping her cool regardless of the circumstances. She seeks to truly understand the needs of her client and delivers above and beyond the call of duty. She is a delightful spirit, a strategist, and forward thinking. Her peculiar talent for anticipating her clients needs is unparalleled. Noel & her team worked the launch of my book party sponsored by Morgan Stanley and Rebecca Minkoff and the night was incredible thanks to her meticulous care. Noel also produced a fundraiser for me that was a huge success!'
     },
     {
       name: 'Patricia Moreno, Best Selling Author, Speaker, & Founder of IntenSATI',
-      image: 'NoellePuck/testimonial2.jpg',
+      image: 'NoelPuck/testimonial2.jpg',
       description: 'Its such a pleasure to get to work with Noel & her team. Create Your Dream Year was a great success and we couldnt have done it without her! She is professional, creative, and passionate about what she does­ making what could be a stressful experience, quite a pleasure!'
     },
     {
       name: 'Meggan Watterson, Best Selling Author, Speaker, & Founder of REVEAL',
-      image: 'NoellePuck/testimonial5.jpg',
+      image: 'NoelPuck/testimonial5.jpg',
       description: 'Noel is an angel & an organizational ninja all rolled into one beautiful package. She is so masterful at creating the flow of an event that I am left to do what I do best without being distracted by the details that used to overwhelm me. If you are looking to create a flawless event, big or small, look no further! Noel Elie Productions is it!'
     },
     {
       name: 'Terri Cole, LCSW Licensed Psychotherapist, Speaker, and Transformation Coach',
-      image: 'NoellePuck/testimonial3.png',
+      image: 'NoelPuck/testimonial3.png',
       description: 'Noel makes me feel so entirely provided for! I never worried once during any of my events. She took care of absolutely every detail which allowed me to just be fully present and entirely there­ a rare and invaluable gift'
     },
     {
       name: 'Joel Readence, Lifecoach',
-      image: 'NoellePuck/testimonial4.jpg',
+      image: 'NoelPuck/testimonial4.jpg',
       description: 'Working a full time job and growing a coaching practice allowed me ZERO time to plan and execute a charity fundraiser for 250+ people. Hiring Noel was the best possible thing I could have done! Her in­depth knowledge of event planning and execution, ability to negotiate contracts and pricing, target and enlist sponsors, and utilize her extensive network of connections ensured a VERY successful event and a new fundraising record for my charity fund! Whether it be a book launch, a fundraiser, or a client retreat, I will never have another event without Noel’s involvement!'
     }
 
@@ -128,7 +134,7 @@ $scope.testimonialGallery = [
   }
 
   for (var i=1; i <= numberOfImages; i++){
-  	var element= $('<span id='+i+' style="background-image: url(NoellePuck/' + i + ')" class="gallerypic"></span>')
+  	var element= $('<span id='+i+' style="background-image: url(NoelPuck/' + i + ')" class="gallerypic"></span>')
   	$(".picture-container").append(element);
   }
 
@@ -136,7 +142,7 @@ $scope.testimonialGallery = [
   	console.log('I was asked to open '+ id);
   	console.log("I am about to open ", imagePath);
 
-  	var imagePath = 'url(NoellePuck/'+id+')';
+  	var imagePath = 'url(NoelPuck/'+id+')';
 
   	$('.full-size-image').css('background-image', imagePath);
   	$('.full-size-image').css('display', 'block');
