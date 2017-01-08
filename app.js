8e9b4beee45b7455d37a7837e816093343c90aea
KatoCode23 <Kalen23Imani@gmail.com>
2017-01-07 22:19:27 -0500

    We have a gallery in progress. Copy Pasta!

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var app = angular.module('app', []);
 app.controller('myController', function($scope){
 $scope.currentCategory = {name: "none"};
 $scope.currentTeam = {name: "none"};
+var numberOfImages= 33;
+var currentImage= null;
 $scope.categoryList = [
   {
     name: 'about',
@@ -124,4 +126,64 @@ $scope.testimonialGallery = [
       window.open(skill.link,'_self');
     }
   }
+
+  for (var i=1; i <= numberOfImages; i++){
+  	var element= $('<span id='+i+' style="background-image: url(NoellePuck/' + i + ')" class="gallerypic"></span>')
+  	$(".picture-container").append(element);
+  }
+
+  openImage= function(id){
+  	console.log('I was asked to open '+ id);
+  	console.log("I am about to open ", imagePath);
+
+  	var imagePath = 'url(NoellePuck/'+id+')';
+
+  	$('.full-size-image').css('background-image', imagePath);
+  	$('.full-size-image').css('display', 'block');
+  	$('.gallery-button').addClass('turned-on');
+
+  	if(currentImage === 1){
+  		$('#prev-button').css('display', 'none');
+  	}else{
+  		$('#prev-button').css('display', 'inline-block');
+  	}
+
+  	if(currentImage === numberOfImages){
+  		$('#next-button').css('display', 'none');
+  	}else{
+  		$('#next-button').css('display', 'inline-block');
+  	}
+  };
+
+  $('.gallerypic').click(function(event){
+  	console.log('YOU SIR JUST CLICKED ', event);
+  	currentImage= event.currentTarget.id;
+  	openImage(currentImage);
+  });
+
+
+  $('.close-button').click(function(event){
+  	$('.full-size-image').css('display', 'none');
+  	$('.gallery-button').removeClass('turned-on');
+
+  	$('#next-button').css('display', 'none');
+  	$('#prev-button').css('display', 'none');
+  });
+
+$('#next-button').click(function(){
+	console.log('kato just clicked next!!!');
+	currentImage = parseInt(currentImage) + 1;
+	openImage(currentImage);
+})
+
+$('#prev-button').click(function(){
+	console.log('kato just clicked prev!!!');
+	currentImage = parseInt(currentImage) - 1;
+	openImage(currentImage);
+})
+
+
+
+
+
 });
