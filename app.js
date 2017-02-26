991292ac664c735c85b21e20b115e29e8bb50127
KatoCode23 <Kalen23Imani@gmail.com>
2017-02-25 21:55:05 -0500

    Clientele Logos

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ $scope.currentCategory = {name: "none"};
 $scope.currentTeam = {name: "none"};
 var numberOfImages= 45;
 var currentImage= null;
+
+var indexOfLogoImages = 228;
 //$scope.imageIndexArr = [];
 //for (i = 1; i <= 31; i++) {
   //$scope.imageIndexArr.push(i);
@@ -50,10 +52,9 @@ $scope.categoryList = [
         },
         {
           name: 'Event Services',
-          about: 'Audio/Visual, Budget Planning, Catering Consultation, Decor Selection, Entertaiment, Floral, Guest Gifts, Hotel Room Blocks, Itineraries, Invitations, Lighting and Sound, Onsite Management, Photography, Program, Rentals,Transportation, Venue Selection & Management, Videography'
+          about: 'Audio/Visual, Budget Planning, Catering Consultation, Decor Selection, Entertaiment, Floral, Guest Gifts, Hotel Room Blocks, Itineraries, Invitations, Lighting and Sound, Onsite Management, Photography, Program, Rentals, Transportation, Venue Selection & Management, Videography'
         }
       ]
-
  },
  {
    name: 'gratitude',
@@ -64,7 +65,7 @@ $scope.categoryList = [
       },
       {
         name: 'clientele',
-        about:'Current & previous clientele:, NY Times Best Selling Author, Kimberly Snyder, NY Times Best Selling Author, Deepak Chopra, Celebrity Raw Chef & Author, Kristina "Fully Raw", NY Times Best Selling Author, Gabrielle Bernstein IMGs Mercedes Benz Fashion Week, The Daily Loves Mastin Kipp, NY Times Best Selling Author, Crazy Sexy Cancers Kris Carr, Meggan Watterson, Sheila Kelley’s S Factor Fitness, IntenSatis Patricia Moreno, Terri Cole Women’s Health Magazine, Mama Glow’s Latham Thomas, Committed Impulse Founder, Josh Pais, Joel Readence, Balloon Management, Vikaz, Soul Ku, GFI Group, Business Meets Spirituality, Alex Jamieson, Gry Sinding, REVEAL Conference (2013 & 2014), NYPS, The William Fund, Soul Empowered Retreats around the world, As seen on Shark Tank, PiperWai All Natural Deodorant, Peace Love and Cancer, Fearless Inventory, Leesa, Sapira'
+        link: 'clientele.html'
       }
     ]
  },
@@ -84,14 +85,12 @@ $scope.categoryList = [
  }
 ];
 
-console.log($scope.categoryList[-1]);
-
 $scope.testimonialGallery = [
     {
       name: 'Kris Carr',
       title: 'New York Times best Selling Author',
       image: 'NoelPuck/testimonial0.jpg',
-      description: 'Working with Noel is a dream come true! Shes an impeccable event producer whos detail oriented, supportive, and forward­ thinking. When I work with Noel, Im able to focus on what matters most, delivering my message. She takes care of the rest!'
+      description: 'Working with Noel is a dream come true! Shes an impeccable event producer whos detail oriented, supportive, and forward thinking. When I work with Noel, Im able to focus on what matters most, delivering my message. She takes care of the rest!'
     },
     {
       name: 'Latham Thomas',
@@ -103,7 +102,7 @@ $scope.testimonialGallery = [
       name: 'Patricia Moreno',
       title: 'Best Selling Author, Speaker, & Founder of IntenSATI',
       image: 'NoelPuck/testimonial2.jpg',
-      description: 'Its such a pleasure to get to work with Noel & her team. Create Your Dream Year was a great success and we couldnt have done it without her! She is professional, creative, and passionate about what she does­ making what could be a stressful experience, quite a pleasure!'
+      description: 'Its such a pleasure to get to work with Noel & her team. Create Your Dream Year was a great success and we couldnt have done it without her! She is professional, creative, and passionate about what she does making what could be a stressful experience, quite a pleasure!'
     },
     {
       name: 'Meggan Watterson',
@@ -115,13 +114,13 @@ $scope.testimonialGallery = [
       name: 'Terri Cole',
       title: 'LCSW Licensed Psychotherapist, Speaker, and Transformation Coach',
       image: 'NoelPuck/testimonial3.png',
-      description: 'Noel makes me feel so entirely provided for! I never worried once during any of my events. She took care of absolutely every detail which allowed me to just be fully present and entirely there­ a rare and invaluable gift'
+      description: 'Noel makes me feel so entirely provided for! I never worried once during any of my events. She took care of absolutely every detail which allowed me to just be fully present and entirely there, a rare and invaluable gift.'
     },
     {
       name: 'Joel Readence',
       title: 'Lifecoach',
       image: 'NoelPuck/testimonial4.jpg',
-      description: 'Working a full time job and growing a coaching practice allowed me ZERO time to plan and execute a charity fundraiser for 250+ people. Hiring Noel was the best possible thing I could have done! Her in­depth knowledge of event planning and execution, ability to negotiate contracts and pricing, target and enlist sponsors, and utilize her extensive network of connections ensured a VERY successful event and a new fundraising record for my charity fund! Whether it be a book launch, a fundraiser, or a client retreat, I will never have another event without Noel’s involvement!'
+      description: 'Working a full time job and growing a coaching practice allowed me ZERO time to plan and execute a charity fundraiser for 250+ people. Hiring Noel was the best possible thing I could have done! Her in depth knowledge of event planning and execution, ability to negotiate contracts and pricing, target and enlist sponsors, and utilize her extensive network of connections ensured a VERY successful event and a new fundraising record for my charity fund! Whether it be a book launch, a fundraiser, or a client retreat, I will never have another event without Noels involvement.'
     }
 ];
 
@@ -156,11 +155,6 @@ $scope.teamGallery = [
    {
      window.open(menu.link,'_self');
    }
-   if(menu.talent && menu.name === 'skills')
-   {
-      console.log("Hey I've been looking for you");
-
-   }
 }
 
   $scope.meetWho = function (skill) {
@@ -175,6 +169,11 @@ $scope.teamGallery = [
   for (var i=1; i <= numberOfImages; i++){
   	var element= $('<span id='+i+' style="background-image: url(NoelPuck/' + i + ')" class="gallerypic"></span>')
   	$(".picture-container").append(element);
+  }
+
+  for (var i=200; i <= indexOfLogoImages; i++){
+    var element= $('<span id='+i+' style="background-image: url(NoelPuck/' + i + '.jpg)" class="logopic"></span>')
+    $(".logo-container").append(element);
   }
 
   openImage= function(id){
